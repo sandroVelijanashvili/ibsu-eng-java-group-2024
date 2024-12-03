ecd34bb99252da0d71dc544ebfad5be095b928a4
IBSU Lecturer <IBSU_Lecturer@ibsu.edu.ge>
2024-12-03 10:47:42 +0400

    add-db-connection

---
+++ src/main/java/ge/ibsu/demo/entities/Address.java
@@ -0,0 +1,33 @@
+package ge.ibsu.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @Column(name =  "address_id")
+    private Long id;
+
+    @Column(name = "address")
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
